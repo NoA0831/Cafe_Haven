1a045f1e167b6ef14e9084d33fcb56e5c2954f25
NoA <noa20040831@gmail.com>
2024-03-06 18:05:14 +0900

    ボタンの四角形を追加

--- Cafe_Haven/ShopScene.cpp
+++ Cafe_Haven/ShopScene.cpp
@@ -10,14 +10,17 @@ void ShopScene::update() {
 void ShopScene::draw() const {
 
 	//研究シーンへのボタン
-	TextureAsset(U"ResearchPoint").scaled(0.06).drawAt(WindowSize.x * 0.3 - 45, WindowSize.y - 40);
-	FontAsset(U"d-font")(U"研究").drawAt(WindowSize.x * 0.3, WindowSize.y - 40, Palette::White);
+	RectF{ WindowSize.x * 0.3 - 85,WindowSize.y - 55,170,60 }.rounded(10).draw(Palette::Black).drawFrame(2, Palette::Black);
+	TextureAsset(U"ResearchPoint").scaled(0.035).drawAt(WindowSize.x * 0.3 - 45, WindowSize.y - 28);
+	FontAsset(U"d-font")(U"研究").drawAt(WindowSize.x * 0.3, WindowSize.y - 30, Palette::White);
 
 	//調理シーンへのボタン
-	TextureAsset(U"Nabe").scaled(0.05).drawAt(WindowSize.x * 0.5 - 45, WindowSize.y - 40);
-	FontAsset(U"d-font")(U"調理").drawAt(WindowSize.x * 0.5, WindowSize.y - 40, Palette::White);
+	RectF{ WindowSize.x * 0.5 - 85,WindowSize.y - 55,170,60 }.rounded(10).draw(Palette::Black).drawFrame(2, Palette::Black);
+	TextureAsset(U"Nabe").scaled(0.07).drawAt(WindowSize.x * 0.5 - 45, WindowSize.y - 33);
+	FontAsset(U"d-font")(U"調理").drawAt(WindowSize.x * 0.5, WindowSize.y - 35, Palette::White);
 
 	//課題シーンへのボタン
-	TextureAsset(U"Jewelry").scaled(0.05).drawAt(WindowSize.x * 0.7 - 45, WindowSize.y - 40);
-	FontAsset(U"d-font")(U"実績").drawAt(WindowSize.x * 0.7, WindowSize.y - 40, Palette::White);
+	RectF{ WindowSize.x * 0.7 - 85,WindowSize.y - 55,170,60 }.rounded(10).draw(Palette::Black).drawFrame(2, Palette::Black);
+	TextureAsset(U"Jewelry").scaled(0.06).drawAt(WindowSize.x * 0.7 - 45, WindowSize.y - 32);
+	FontAsset(U"d-font")(U"実績").drawAt(WindowSize.x * 0.7, WindowSize.y - 35, Palette::White);
 }
